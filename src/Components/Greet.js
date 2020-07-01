29e2158285aff2025d58a3970c8bfce27033aa72
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-07-01 12:14:37 +0530

    ADDED: Sample project for AWS

---
+++ src/Components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function greet(props){
+   
+return <h2>Hello {props.name}</h2>
+}
+
+export default greet
---
+++ src/Components/GreetClassComp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class GreetClassComp extends React.Component{
+    render(){
+    return <h1>Hello Sam {this.props.name}</h1>
+    }
+}
+export default GreetClassComp
